f841a057e6ffc56c19acb0ffd455b164c8351b51
juan <juanjomoss@gmail.com>
2025-06-26 12:42:29 -0500

    Commit made from VS Code Azure Static Web Apps

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -1,5 +1,6 @@
 export namespace Config {
-    export const LOGIC_URL = 'http://127.0.0.1:3000/'
+    // export const LOGIC_URL = 'http://127.0.0.1:3000/'
+    export const LOGIC_URL = 'https://azure-inv-jm-be-epcaathjhjh8bhcp.eastus2-01.azurewebsites.net/'
     export const defaultConfig = {
         headers: {
             'Content-Type': 'application/json',
