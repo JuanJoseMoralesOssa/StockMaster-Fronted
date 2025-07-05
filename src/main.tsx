d1458536a1fbe9fddf4f25e4f9bb452350b7d67b
juan <juanjomoss@gmail.com>
2025-07-05 08:26:31 -0500

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-    <section>
-        {
-            import.meta.env.DEV &&
-            <StrictMode>
-                <App />
-            </StrictMode>
-        }
-        {!import.meta.env.DEV && <App />}
-    </section>
+    <StrictMode>
+        <App />
+    </StrictMode>
 )
